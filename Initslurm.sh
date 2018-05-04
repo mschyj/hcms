dee5f30982f51d9d289b64994513b782e7952a7a
root <root@hwc.(none)>
2018-05-04 09:52:04 +0800

    chenyjie-update script

---
+++ Initslurm.sh
@@ -0,0 +1,1 @@
+python $HOME/.hwcc/script/init_ssh.py slurm-master001 >> output &
--- resize_slurm.sh
+++ resize_slurm.sh
@@ -1,1 +1,1 @@
-elasticluster -vvv -c /root/hwc/cfg/config -s /root/hwc/cfg/storage resize -a 1:worker slurm --yes > output 2>&1 &
+hwcc -vvv -c ~/.hwcc/cfg/config -s ~/.hwcc/cfg/storage resize -a 1:worker slurm --yes > output 2>&1 &
