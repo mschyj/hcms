4dc1cd6e2ff0ec8c1aa7657824e25fbeb5d458e0
root <root@hwc.(none)>
2018-05-16 12:25:19 +0800

    chenyjie-add encrypt script

---
+++ script/encrypt.py
@@ -0,0 +1,39 @@
+#!/user/bin/env python
+
+import sys
+import os
+import ConfigParser
+
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+class AESCrypto():
+    def __init__(self,key):
+        self.key = key
+        self.mode = AES.MODE_CBC
+
+    def encrypt(self,text):
+        if len(text)%16!=0:
+            text=text+str((16-len(text)%16)*'+')
+        cryptor = AES.new(self.key,self.mode,b'0000000000000000')
+        self.ciphertext = cryptor.encrypt(text)
+        return b2a_hex(self.ciphertext)
+
+    def decrypt(self,text):
+        cryptor = AES.new(self.key,self.mode,b'0000000000000000')
+        plain_text  = cryptor.decrypt(a2b_hex(text))
+        return plain_text.rstrip('+')
+if __name__ == '__main__':
+    username = raw_input("Please input your username of your cloud provider: ")
+    password = raw_input("Please input your password of your cloud provider: ")
+    pc = AESCrypto('1234567890123456') 
+    sec_username = pc.encrypt(username)
+    sec_password = pc.encrypt(password)
+    print "The encrypted username is: " + sec_username
+    print "The encrypted password is: " + sec_password
+    home=os.environ['HOME']
+    config=ConfigParser.ConfigParser()
+    config.read(home + "/.hwcc/config")
+    config.set("cloud/hwc", "username", sec_username)
+    config.set("cloud/hwc", "password", sec_password)
+    config.write(open(home + "/.hwcc/config","w")) 
+    print "The config file has been updated"
