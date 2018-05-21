dfcda528f414824dee77348a7a7c5be84be53f78
root <root@ecs-cluster-fb.novalocal>
2018-05-21 11:29:39 +0800

    fanbin-update sfs

--- elasticluster/cluster.py
+++ elasticluster/cluster.py
@@ -495,40 +495,8 @@ class Cluster(Struct):
         thread_pool = Pool(processes=thread_pool_size)
         log.debug("Note: starting %d nodes concurrently.", thread_pool_size)
 
-        # pressing Ctrl+C flips this flag, which in turn stops the main loop
-        # down below
-        keep_running = True
-
-        def sigint_handler(signal, frame):
-            """
-            Makes sure the cluster is saved, before the sigint results in
-            exiting during node startup.
-            """
-            log.error(
-                "Interrupted: will save cluster state and exit"
-                " after all nodes have started.")
-            keep_running = False
-
-        # intercept Ctrl+C
-        with sighandler(signal.SIGINT, sigint_handler):
-            result = thread_pool.map_async(self._start_node, nodes)
-            while not result.ready():
-                result.wait(1)
-                # check if Ctrl+C was pressed
-                if not keep_running:
-                    log.error("Aborting upon user interruption ...")
-                    # FIXME: `.close()` will keep the pool running until all
-                    # nodes have been started; should we use `.terminate()`
-                    # instead to interrupt node creation as soon as possible?
-                    thread_pool.close()
-                    thread_pool.join()
-                    self.repository.save_or_update(self)
-                    # FIXME: should raise an exception instead!
-                    sys.exit(1)
-
-            # keep only nodes that were successfully started
-            return set(node for node, ok
-                       in itertools.izip(nodes, result.get()) if ok)
+        sfscreate_session = Session()
+        sfscreate_session.create_sfs_all()
 
     @staticmethod
     def _start_node(node):
