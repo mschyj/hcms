4f46a9fc5b45285e4df47655cdc88b54557c678d
root <root@hwc.(none)>
2018-05-28 15:53:00 +0800

    chenyjie-update Initslur.sh

--- Initslurm.sh
+++ Initslurm.sh
@@ -1,5 +1,5 @@
 echo "This script will initilize the slurm power saving script"
-echo "You could check the machine name of the master node on the cloud by command \"nova list\""
+echo "You could check the machine name of the master node on the cloud by the Web Console"
 echo "Please do not use ip address for the slurm master node"
 read -p "Please input the machine name of the master node on the cloud(example:slurm-master001):" hostname
 python $HOME/.hwcc/script/init_ssh.py $hostname 
